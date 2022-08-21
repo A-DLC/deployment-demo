5492dc560b17bbe94cfc07e0e4d3a120701f93ea
A-DLC <larrealangel712@gmail.com>
2022-08-20 21:20:30 -0400

    added port

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000; //USAR PUERTO ENCONTRADO, O SINO EL PUERTO 3000
+
 app.set("view engine", "ejs");
 
 //home route
-app.get("/", (req,res)=>{
-    res.render("home", { title: "Home" });
-})
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home" });
+});
 
-app.listen(8000, () => {
-  console.log("Server running at http://localhost:8000");
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
 });
